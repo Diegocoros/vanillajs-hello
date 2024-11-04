4d329c81ba9ca1050202f0ad086c096fd5edb22d
Diegocoros <diegocoros22@gmail.com>
2024-11-04 20:03:43 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,30 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+// Función para generar una excusa aleatoria
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Seleccionar elemento aleatorio de cada arreglo
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Concatena elementos seleccionados en una excusa
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+
+// Ejecuta la función y establece el resultado en el elemento #excuse al cargar la página
+window.onload = () => {
+  document.getElementById("excuse").innerHTML = generateExcuse();
+};
